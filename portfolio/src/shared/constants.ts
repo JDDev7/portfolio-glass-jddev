1f294a568bdc3ebdc465a22e3d0b208da2e8b5b4
Jose Diego Moreno <jdmorenopulido@gmail.com>
2024-10-23 19:23:08 +0200

    intento fix link cv

--- portfolio/src/shared/constants.ts
+++ portfolio/src/shared/constants.ts
@@ -32,8 +32,8 @@ export const descriptions = {
 };
 
 
-export const spCvUrl = "../assets/CV_Jose_Diego_Moreno_2024[Spanish].pdf";
-export const enCvUrl = "../assets/CV_Jose_Diego_Moreno_2024[English].pdf";
+export const spCvUrl = "../../public/CV_Jose_Diego_Moreno_2024[Spanish].pdf";
+export const enCvUrl = "../../public/CV_Jose_Diego_Moreno_2024[English].pdf";
 export const mail = "https://mail.google.com/mail/?view=cm&fs=1&to=jdmorenopulido@gmail.com"
 
 // job texts
